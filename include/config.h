ebd2265e9fdc935a2cf9d10d53c4b54e95726010
Arvisucbe <guillaume.dallechallamel@sfr.fr>
2024-03-05 23:53:29 +0100

    configuration pinout

---
+++ include/config.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Arduino.h"
+
+
+//Pin Servo moteur
+#define PIN_SERVOMOTEUR_1 7
+#define PIN_SERVOMOTEUR_2 6
+#define PIN_SERVOMOTEUR_3 9
+#define PIN_SERVOMOTEUR_4 8
+#define PIN_SERVOMOTEUR_5 11
+#define PIN_SERVOMOTEUR_6 10
+#define PIN_SERVOMOTEUR_7 13
+#define PIN_SERVOMOTEUR_8 12
+
+//Pin Moteur pas a pas
+#define PIN_STEPPER_STEP_1 3
+#define PIN_STEPPER_DIR_1 25
+#define PIN_STEPPER_ENABLE_1 28
+
+#define PIN_STEPPER_STEP_2 2
+#define PIN_STEPPER_DIR_2 27
+#define PIN_STEPPER_ENABLE_2 26
+
+#define PIN_STEPPER_STEP_3 5
+#define PIN_STEPPER_DIR_3 29
+#define PIN_STEPPER_ENABLE_3 24
+
+//Pin Actionneur
+#define PIN_ACTIONNEUR_1 4
+#define PIN_ACTIONNEUR_2 42
+#define PIN_ACTIONNEUR_3 43
+
+
+//Pin Moteur dc
+#define PIN_MOTEURDC_REVERSE_1 45
+#define PIN_MOTEURDC_FORWARD_1 46
+
+#define PIN_MOTEURDC_REVERSE_2 44
+#define PIN_MOTEURDC_FORWARD_2 52
+
+
+//pin capteur
+#define PIN_CAPTEUR_1 32
+#define PIN_CAPTEUR_2 33
+#define PIN_CAPTEUR_3 34
+#define PIN_CAPTEUR_4 35
+#define PIN_CAPTEUR_5 36
+#define PIN_CAPTEUR_6 37
+#define PIN_CAPTEUR_7 38
+#define PIN_CAPTEUR_8 39
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,83 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
+#include <Servo.h>
+#include <Wire.h>
+#include "config.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
+
+AccelStepper stepper1(AccelStepper::DRIVER, PIN_STEPPER_STEP_1, PIN_STEPPER_DIR_1);
+AccelStepper stepper2(AccelStepper::DRIVER, PIN_STEPPER_STEP_2, PIN_STEPPER_DIR_2);
+
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  servo1.attach(PIN_SERVOMOTEUR_1);
+  servo2.attach(PIN_SERVOMOTEUR_2);
+  servo3.attach(PIN_SERVOMOTEUR_3);
+  servo4.attach(PIN_SERVOMOTEUR_4);
+  servo5.attach(PIN_SERVOMOTEUR_5);
+  servo6.attach(PIN_SERVOMOTEUR_6);
+  servo7.attach(PIN_SERVOMOTEUR_7);
+  servo8.attach(PIN_SERVOMOTEUR_8);
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
+  servo4.write(0);
+  servo5.write(0);
+  servo6.write(0);
+  servo7.write(0);
+  servo8.write(0);
+
+  pinMode(PIN_STEPPER_ENABLE_1, OUTPUT);
+  pinMode(PIN_STEPPER_ENABLE_2, OUTPUT);
+  digitalWrite(PIN_STEPPER_ENABLE_1,HIGH);
+  digitalWrite(PIN_STEPPER_ENABLE_2,HIGH);
+
+  pinMode(PIN_ACTIONNEUR_1, OUTPUT);
+  pinMode(PIN_ACTIONNEUR_2, OUTPUT);
+  pinMode(PIN_ACTIONNEUR_3, OUTPUT);
+  digitalWrite(PIN_ACTIONNEUR_1,LOW);
+  digitalWrite(PIN_ACTIONNEUR_2,LOW);
+  digitalWrite(PIN_ACTIONNEUR_3,LOW);
+
+  pinMode(PIN_MOTEURDC_REVERSE_1, OUTPUT);
+  pinMode(PIN_MOTEURDC_FORWARD_1, OUTPUT);
+  digitalWrite(PIN_MOTEURDC_REVERSE_1,LOW);
+  digitalWrite(PIN_MOTEURDC_FORWARD_1,LOW);
+  pinMode(PIN_MOTEURDC_REVERSE_2, OUTPUT);
+  pinMode(PIN_MOTEURDC_FORWARD_2, OUTPUT);
+  digitalWrite(PIN_MOTEURDC_REVERSE_2,HIGH);
+  digitalWrite(PIN_MOTEURDC_FORWARD_2,HIGH);
+
+  pinMode(PIN_CAPTEUR_1, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_2, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_3, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_4, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_5, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_6, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_7, INPUT_PULLUP);
+  pinMode(PIN_CAPTEUR_8, INPUT_PULLUP);
+
+  Serial.begin(115200);
+  Wire.begin(100);
+  Wire.setTimeout(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  stepper1.run();
+  stepper2.run();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
+
+
 }

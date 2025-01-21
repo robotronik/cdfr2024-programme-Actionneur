db679b54cdffa977cf778f5f6337786055c75e5a
SpeederW <killersnope@gmail.com>
2025-01-21 15:11:30 +0100

    cleaning part 2 (yay)

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,23 @@
 // TODO: move these defines later
 #define SERVO_MAX_VALUE 180
 #define SERVO_MIN_VALUE 0
+#define SERVO_COUNT 7
+#define STEPPER_COUNT 3
 
+/* TODO: Remove
 AccelStepper stepper1(AccelStepper::DRIVER, PIN_STEPPER_STEP_1, PIN_STEPPER_DIR_1);
 AccelStepper stepper2(AccelStepper::DRIVER, PIN_STEPPER_STEP_2, PIN_STEPPER_DIR_2);
 AccelStepper stepper3(AccelStepper::DRIVER, PIN_STEPPER_STEP_3, PIN_STEPPER_DIR_3);
+*/
 
+// idk if this is ok
+AccelStepper steppers[STEPPER_COUNT] = { 
+  {AccelStepper::DRIVER, PIN_STEPPER_STEP_1, PIN_STEPPER_DIR_1};
+  {AccelStepper::DRIVER, PIN_STEPPER_STEP_2, PIN_STEPPER_DIR_2};
+  {AccelStepper::DRIVER, PIN_STEPPER_STEP_3, PIN_STEPPER_DIR_3};
+}
+
+/* TODO: Remove
 servoControl servo1;
 servoControl servo2(true); // ??
 servoControl servo3;
@@ -21,6 +33,13 @@ servoControl servo4;
 servoControl servo5;
 servoControl servo6;
 servoControl servo7;
+*/
+
+servoControl *servos[SERVO_COUNT];
+for(int i=0; i<SERVO_COUNT; i++) {
+  servos[i] = new servoControl; 
+}
+
 
 uint8_t onReceiveData[BUFFERONRECEIVESIZE];
 uint8_t onRequestData[BUFFERONREQUESTSIZE];
@@ -33,20 +52,20 @@ void setup() {
   Serial.begin(115200);
   Serial.println("start 2");
 
-  initServo(servo1, PIN_SERVOMOTEUR_1, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo2, PIN_SERVOMOTEUR_2, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo3, PIN_SERVOMOTEUR_3, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo4, PIN_SERVOMOTEUR_4, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo4, PIN_SERVOMOTEUR_5, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo4, PIN_SERVOMOTEUR_6, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
-  initServo(servo4, PIN_SERVOMOTEUR_7, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[0], PIN_SERVOMOTEUR_1, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[1], PIN_SERVOMOTEUR_2, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[2], PIN_SERVOMOTEUR_3, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[3], PIN_SERVOMOTEUR_4, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[4], PIN_SERVOMOTEUR_5, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[5], PIN_SERVOMOTEUR_6, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
+  initServo(servos[6], PIN_SERVOMOTEUR_7, SERVO_MIN_VALUE, SERVO_MAX_VALUE, 0);
 
   initPin(PIN_STEPPER_SLEEP, false);
   initPin(PIN_STEPPER_RESET, false);
   delay(1); // ??
-  initStepper(stepper1, PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
-  initStepper(stepper2, PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
-  initStepper(stepper3, PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
+  initStepper(steppers[0], PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
+  initStepper(steppers[1], PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
+  initStepper(steppers[2], PIN_STEPPER_ENABLE_1, DEFAULT_MAX_SPEED, DEFAULT_MAX_ACCEL);
 
   initPin(PIN_ACTIONNEUR_1, false);
   initPin(PIN_ACTIONNEUR_2, false);
@@ -78,19 +97,12 @@ void setup() {
 }
 
 void loop() {
-  stepper1.run();
-  stepper2.run();
-  stepper3.run();
-  servo1.run();
-  servo2.run();
-  servo3.run();
-  servo4.run();
-  servo5.run();
-  servo6.run();
-  servo7.run();
-  // loop_test();
-  // servo8.run();
-  //delay(100);
+  for(int servoNb=0; servoNb<SERVO_COUNT; servoNb++) {
+    servos[servoNb].run();
+  }
+  for(int stepperNb=0; stepperNb<STEPPER_COUNT; stepperNb++) {
+    steppers[stepperNb].run();
+  }
 }
 
 void receiveEvent(int numBytes) {
@@ -106,133 +118,65 @@ void receiveEvent(int numBytes) {
    
   }
 
-  int commande;
-  arrayToParameter(onReceiveData,BUFFERONRECEIVESIZE,"1%d",&commande);
+  // still kinda ugly
+  int number;  
+  int position = 0; 
+  arrayToParameter(onReceiveData,BUFFERONRECEIVESIZE,"1%d",&number);
+  arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
 
-  switch (commande)
-  {
-  case 1 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo1.write(position);
-    break;
-  }
-
-  case 2 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo2.write(position);
-    break;
-  }
-
-  case 3 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo3.write(position);
-    break;
-  }
-
-  case 4 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo4.write(position);
-    break;
-  }
-
-  case 5 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo5.write(position);
-    break;
-  }
-
-  case 6 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo6.write(position);
-    break;
-  }
-
-  case 7 :{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    servo7.write(position);
-    break;
-  }
-
-  // case 8 :{
-  //   int position = 0; 
-  //   arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-  //   servo8.write(position);
-  //   break;
-  // }
-
-  case 11:{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    stepper1.moveTo(position);
-    break;
-  }
-
-  case 12:{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    stepper2.moveTo(position);
-    break;
-  }
-
-  case 13:{
-    int position = 0; 
-    arrayToParameter(onReceiveData+1,BUFFERONRECEIVESIZE,"2%d",&position);
-    stepper3.moveTo(position);
-    break;
-  }
-  case 21:
-    digitalWrite(PIN_STEPPER_ENABLE_1,LOW);
-    break;
+  if(number > STEPPER_COUNT + 10) { // servos can take ids from 1 to 10, and steppers 10 to 20. above we do other stuff
+    switch(number) { // old code here still, dunno if it can be simplified
+      case 21:
+        digitalWrite(PIN_STEPPER_ENABLE_1,LOW);
+        break;
   
-  case 22:
-    digitalWrite(PIN_STEPPER_ENABLE_1,HIGH);
-    break;
+      case 22:
+        digitalWrite(PIN_STEPPER_ENABLE_1,HIGH);
+        break;
   
-  case 23:
-    digitalWrite(PIN_STEPPER_ENABLE_2,LOW);
-    break;
+      case 23:
+        digitalWrite(PIN_STEPPER_ENABLE_2,LOW);
+        break;
   
-  case 24:
-    digitalWrite(PIN_STEPPER_ENABLE_2,HIGH);
-    break;
+      case 24:
+        digitalWrite(PIN_STEPPER_ENABLE_2,HIGH);
+        break;
   
-  case 25:
-    digitalWrite(PIN_STEPPER_ENABLE_3,LOW);
-    break;
+      case 25:
+        digitalWrite(PIN_STEPPER_ENABLE_3,LOW);
+        break;
   
-  case 26:
-    digitalWrite(PIN_STEPPER_ENABLE_3,HIGH);
-    break;
+      case 26:
+        digitalWrite(PIN_STEPPER_ENABLE_3,HIGH);
+        break;
 
-  case 31:
-    digitalWrite(PIN_CAPTEUR_4,HIGH);
-    break;
+      case 31:
+        digitalWrite(PIN_CAPTEUR_4,HIGH);
+        break;
 
-  case 32:
-    digitalWrite(PIN_CAPTEUR_4,LOW);
-    break;
+      case 32:
+        digitalWrite(PIN_CAPTEUR_4,LOW);
+        break;
 
-  case 33:
-    digitalWrite(PIN_CAPTEUR_5,HIGH);
-    break;
+      case 33:
+        digitalWrite(PIN_CAPTEUR_5,HIGH);
+        break;
 
-  case 34:
-    digitalWrite(PIN_CAPTEUR_5,LOW);
-    break;
+      case 34:
+        digitalWrite(PIN_CAPTEUR_5,LOW);
+        break;
 
-  default:
-    break;
+      default: break;
+    }
   }
+  else if(number > SERVO_COUNT) 
+    steppers[number - 1].write(position);
+  else 
+    servos[number - 1].write(position);
+
 }
 
-void requestEvent(){
+void requestEvent(){ // i have no idea what this really does yet
   switch (onReceiveData[0])
   {
   case 100 :
@@ -272,11 +216,6 @@ void requestEvent(){
 
   case 107 :
     parameterToArray(onRequestData,BUFFERONREQUESTSIZE,"2%d",!digitalRead(PIN_CAPTEUR_8));
-    lenghtOnRequest = 2;
-    break;
-
-  case 109:
-    parameterToArray(onRequestData,BUFFERONREQUESTSIZE,"2%d",!digitalRead(PIN_CAPTEUR_PLANTE));
     lenghtOnRequest = 2;
     break;
   
